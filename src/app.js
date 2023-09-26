c7ee9fdfd7548f68d27571547e105b07f4079733
RobertaColle <143618199+RobertaColle@users.noreply.github.com>
2023-09-26 10:42:30 +0200

    update the APIkey

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
